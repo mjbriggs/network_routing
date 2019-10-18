87145c970b92e29f3d4de511bb9587f6ad89af51
Michael Briggs <mjbriggs@icloud.com>
2019-10-18 15:53:26 -0600

    updates prev and dist arr

--- NetworkRoutingSolver.py
+++ NetworkRoutingSolver.py
@@ -47,15 +47,39 @@ class NetworkRoutingSolver:
         self.dist_arr[self.source] = 0
 
         array_heap.makeQueue(self.network.getNodes(), self.dist_arr, self.prev_arr)
-        
+        nodes = self.network.getNodes()
+        print(nodes)
         while array_heap.length() > 0:
             node_index = array_heap.deleteMin()
-            node = self.network.nodes[self.source]
-            node = self.network.nodes[node_index]
+            # node = self.network.nodes[self.source]
+            node = nodes[node_index]
             for e in range(len(node.neighbors)):
-                print(node.neighbors[e])
-                # if array_heap.dist_array[node_index] > array_heap.dist_array[edge]
+                neighbor_index = node.neighbors[e].dest.node_id
+                neighbor_node = node.neighbors[e].dest
+                edge = node.neighbors[e]
+                print("node_index ", node_index)
+                print("node_edge_index, ", e)
+                # print("node ", node)
+                print("neighbor_index ", neighbor_index)
+                # print("neighbor_node ", neighbor_node)
+                # print("edge ", edge)
+                if array_heap.dist_array[neighbor_index] > array_heap.dist_array[node_index] + edge.length:
+                    print(array_heap.dist_array[neighbor_index], " > ", array_heap.dist_array[node_index], " + ", edge.length)
+                    array_heap.dist_array[neighbor_index] = array_heap.dist_array[node_index] + edge.length
+                    array_heap.prev_array[neighbor_index] = node_index
+                    print(array_heap.dist_array)
+                    print(array_heap.prev_array)
+                elif(array_heap.dist_array[neighbor_index] < 0):
+                    print(array_heap.dist_array[neighbor_index], " is now  ", array_heap.dist_array[node_index], " + ", edge.length)
+                    array_heap.dist_array[neighbor_index] = array_heap.dist_array[node_index] + edge.length
+                    array_heap.prev_array[neighbor_index] = node_index
+                    print(array_heap.dist_array)
+                    print(array_heap.prev_array)
+                    
 
+
+        print(array_heap.dist_array)
+        print(array_heap.prev_array)
         array_heap.decreaseKey(None)
         array_heap.deleteMin()
         # for i in range(self.source):

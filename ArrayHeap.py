4894fde39ca53db8f93d08e74ebfc01a978c2d75
Michael Briggs <mjbriggs@icloud.com>
2019-10-19 16:58:48 -0600

    deletes correct minimum from heap

--- ArrayHeap.py
+++ ArrayHeap.py
@@ -29,22 +29,37 @@ class ArrayHeap(NetworkHeap):
         print("prev_array ", self.prev_array)
         if len(self.heap) < 1:
             return -1
-        min_index = -5
+        # this just signals that we are at the start of the loop, 
+        min_index = -1
         min = -1
-
-        for i in range(len(self.heap)):
-            if min_index == -5:
+        i = 0             
+        while i < len(self.heap):
+            if min_index < 1:
                 min_index = self.heap[i]
                 min = self.dist_array[min_index]
-            else:
-                if min == 0:
+            print("i == ", i, " heap length is ", len(self.heap))
+            print("at min_index ", min_index, " min distance is ", min)
+            if min == 0:
                     print("at min_index ", min_index, " min distance is ", min)
                     self.heap.remove(min_index)
                     return min_index
-                if min > self.dist_array[i]:
-                    min_index = self.heap[i]
-                    min = self.dist_array[min_index]
-            print("at min_index ", min_index, " min distance is ", min)
+            elif self.dist_array[self.heap[i]] == -1:
+                print("distance value is -1")
+            elif min == -1 and self.dist_array[self.heap[i]] > 0:
+                print("setting new min at ", self.heap[i])
+                min_index = self.heap[i]
+                min = self.dist_array[min_index]
+            elif min > self.dist_array[self.heap[i]]:
+                print("setting new min at ", self.heap[i])
+                min_index = self.heap[i]
+                min = self.dist_array[min_index]
+            i += 1
+
+        # for i in range(len(self.heap)):
+        #     if min_index == -5: # start of loop, set min_index and min to first element
+               
+        #     else:
+                
         
         self.heap.remove(min_index)
         return min_index
@@ -53,5 +68,5 @@ class ArrayHeap(NetworkHeap):
         print("decreaseKey")
 
     def length(self):
-        print(len(self.heap))
+        print("heap length is ", len(self.heap))
         return len(self.heap)
